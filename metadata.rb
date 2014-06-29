9d817a284db38ffdbb8b2890e317eb8ff234c5e1
Jonathan Werrett <jonathan@werrett.com.au>
2014-06-29 20:17:28 +0800

    Updated Vagrant config to work with Vagrant 1.5.x

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'sqlmap'
+name             'sqlmap-cookbook'
 maintainer       'Jonathan Werrett'
 maintainer_email 'jonathan@werrett.hk'
 license          'MIT'
 description      'Installs and Configures Sqlmap'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.1'
